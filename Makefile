c8996930959b6c41ff9ab312e69c74e33c312607
Wator Vapor <watorvapor@gmail.com>
2015-09-22 22:58:08 +0900

    skelton

--- Makefile
+++ Makefile
@@ -5,5 +5,5 @@ build:
 	cd $(OBJ) && cmake ../
 	cd $(OBJ) && make
 test:build
-	cd $(OBJ) && test/run/test
+	cd $(OBJ) && test/test
 	
---
+++ test/main.cpp
@@ -0,0 +1,5 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#include <vector>
+#include <boost/test/included/unit_test.hpp>
+
