364bd280f1384ebc4f4dcd0720542aeae19e51a6
Wator Vapor <watorvapor@gmail.com>
2015-09-22 23:01:53 +0900

    skelton

--- test/main.cpp
+++ test/main.cpp
@@ -2,4 +2,12 @@
 #define BOOST_TEST_MAIN
 #include <vector>
 #include <boost/test/included/unit_test.hpp>
+BOOST_AUTO_TEST_SUITE(MasterSuite)
+
+BOOST_AUTO_TEST_CASE(TestFoo)
+{
+    BOOST_CHECK(0==0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
 
