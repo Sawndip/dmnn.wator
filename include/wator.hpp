cda98301f3d4dbd1467d93e74f993eddeda112fd
Wator Vapor <watorvapor@gmail.com>
2015-10-16 09:59:35 +0900

    Update wator.hpp

--- include/wator.hpp
+++ include/wator.hpp
@@ -30,4 +30,5 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 #include <wator/net.hpp>
 #include <wator/layer.hpp>
+#include <wator/blob.hpp>
 #include <wator/layer_image.hpp>
